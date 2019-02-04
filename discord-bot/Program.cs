9bede26666f624b7ac84d826472ce2618060bede
Michael Sloan <mikesloan47@gmail.com>
2019-02-03 22:54:16 -0500

    Add VS solution

---
+++ discord-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discord_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
